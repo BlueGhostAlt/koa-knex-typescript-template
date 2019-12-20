36ebd3d30e88a61c4839afa5a5b232801b9ac259
BlueGhostGH <engisoftleaderoff@gmail.com>
2019-12-20 14:45:47 +0200

    feat: added a basic koa app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello Koa community!");
+import Koa from "koa";
+import Router from "koa-router";
+
+import logger from "koa-logger";
+import json from "koa-json";
+import bodyParser from "koa-bodyparser";
+
+const app = new Koa();
+
+const router = new Router();
+
+const port = process.env.PORT || 8090;
+
+app.use(bodyParser());
+app.use(json());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(logger());
+}
+
+app.use(router.routes()).use(router.allowedMethods());
+
+export const server = app.listen(port, () => {
+    console.info(`Koa app started and listening on port ${port}!`);
+});
